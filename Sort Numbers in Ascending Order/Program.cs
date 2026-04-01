623dae8954cd7d8ea58b3df1b004fa74ac6443b6
Oskar Swat <oskar.swat@rossmann.com.pl>
2026-04-01 13:13:44 +0200

    https://edabit.com/challenge/RocWAnyqu5J4fiZxS

---
+++ Sort Numbers in Ascending Order/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+public class Program
+    {
+        public static int[] SortNumsAscending(int[] arr)
+        {
+            if (arr == null || arr.Length == 0) return new int[0];
+            return arr.OrderBy(x => x).ToArray();
+        }
+    }
