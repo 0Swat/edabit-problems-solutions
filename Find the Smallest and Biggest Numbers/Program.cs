6c0a83f62fc426e50c0ce5b29ad0ebd36b012b5d
Oskar Swat <oskar.swat@rossmann.com.pl>
2026-04-01 13:01:10 +0200

    https://edabit.com/challenge/kMWmiNJM4szSv7dLd

---
+++ Find the Smallest and Biggest Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+public class Program 
+{
+    public static double[] FindMinMax(double[] values)
+    {
+        double min = values[0];
+        double max = values[0];
+        foreach (var value in values)
+        {
+            if (value > max) max = value;
+            if (value < min) min = value;
+        }
+        return new double[] { min, max };
+    }
+}
